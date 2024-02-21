878b8f2339c0256008d12e5219813e867ed94554
cpaciencia <cpaciencia@hotmail.com>
2024-02-21 10:20:32 +0100

    delete code not need

--- src/main/java/com/tenantevaluation/training/movies/web/resources/MoviesController.java
+++ src/main/java/com/tenantevaluation/training/movies/web/resources/MoviesController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/movies")
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class MoviesController {
 
 
